8a7cec369c12b3dcbd7b95cac5a8428e10540d94
yaragtz <syajaira68@gmail.com>
2025-12-02 11:13:46 -0600

    Segundo commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+  use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();}
+    Category::create(['name' => 'generica', 'description' => 'generica']);
+    Category::create(['name' => 'economica', 'description' => 'economica']);
+    Category::create(['name' => 'premium', 'description' => 'premium']);
+  }
 }
